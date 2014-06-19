1f3b7528fdcc7e603023f0cc6dac5562aa1ec9ce
Arun Mittal <mittal.talk@gmail.com>
2014-06-19 11:05:52 +0530

    created Layout

--- static/vt/js/ngapp/controller.js
+++ static/vt/js/ngapp/controller.js
@@ -2,10 +2,21 @@
 
 var controllers = angular.module("nwayApp.controllers", ["ui.bootstrap"]);
 
-controllers.controller("DashboardTabsCtrl", ["$scope",
-      function($scope){
-      	  $scope.tabs = [
-    		{ title:'List', content:'Dynamic content 1' },
-    		{ title:'New', content:'Dynamic content 2' }
-  			];  
+controllers.controller("DashboardTabsCtrl", ["$scope", function($scope){
+      	   
 }]);  
+
+controllers.controller("AddItemCtrl", ["$scope", "$http", function($scope, $http){
+  $scope.status = {
+      isopen: false
+    };
+  
+	function getCategories(){
+		$http.get("/api/products/").success(function(response){
+			$scope.productCategories=response;
+		});
+	}
+	getCategories();
+	
+}]);  
+
